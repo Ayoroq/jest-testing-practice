100440497a7bee42c2d46a59c4b064b8629e35a5
Ayo <Ayoroq@gmail.com>
2025-09-10 13:07:13 -0400

    Added gitignore for some of the files and included the reverstring test

--- practice.test.js
+++ practice.test.js
@@ -1,6 +1,11 @@
-const { capitalize } = require('./practice');
+const { capitalize,reverseString } = require('./practice');
 
 test('returns the a capitalized word', () => {
   expect(capitalize('hello')).toBe('Hello');
   expect(capitalize('world')).toBe('World');
+});
+
+test('returns the a reversed word', () => {
+  expect(reverseString('hello')).toBe('olleh');
+  expect(reverseString('world')).toBe('dlrow');
 });
